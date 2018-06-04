46f6ff0459c8767e713ae1e77b96a1e349f6ae83
kasulejoseph <kasule06joseph@gmail.com>
2018-06-04 17:20:06 +0300

    changes

---
+++ my_chatbot.py
@@ -0,0 +1,31 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from gtts import gTTS
+from pygame import mixer
+import speech_recognition as sr
+import os
+
+r = sr.Recognizer()
+mixer.init()
+
+bot = ChatBot('Bot')
+bot.set_trainer(ListTrainer)
+
+for files in os.listdir('english'):
+    data = open('english/' +files, 'r').readlines()
+    bot.train(data)
+
+while True:
+    message = input('You: ')
+    if message.strip() != 'Bye':
+        reply = bot.get_response(message)
+
+        tts = gTTS('' +str(reply), lang = 'en')
+        tts.save('kasule.mp3')
+        mixer.music.load('kasule.mp3')
+        mixer.music.play()
+
+        print('ChatBot :', reply)
+    if message.strip() == 'Bye':
+        print('ChatBot : Bye')
+        break
